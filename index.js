bf5af928ef7e3444749ac184c62427aa3fbcc4f1
ilhamnurf <ilhamnurfirdaus2722@gmail.com>
2022-01-17 14:54:30 +0700

    register

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require ('dotenv');
-const res = require('express/lib/response');
-const req = require('express/lib/request');
+const dotenv = require("dotenv");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
+const router = require("./routes");
 dotenv.config();
-const port= process.env.PORT ||800;
+const port = process.env.PORT || 8000;
 
- app.get("/",(req,res)=>{
-     res.send('selamat JALAN')
- })
+require("dotenv").config();
 
- app.listen(port, ()=>{
-    console.log(`Walked on 1945 By Mr.Adolf H.`);
-})
+app.use(express.json());
+app.use(router);
+app.listen(port, () => {
+  console.log(`Walked on 1945 By Mr.Adolf H. ${port}`);
+});
+
