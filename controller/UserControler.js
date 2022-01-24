c12ae545354da8ae37be237d423d8bd6d7b7aa81
ilhamnurf <ilhamnurfirdaus2722@gmail.com>
2022-01-24 14:42:42 +0700

    senen3

--- controller/UserControler.js
+++ controller/UserControler.js
@@ -46,4 +46,93 @@ const detail = async (req, res) => {
   }
 };
 
-module.exports = { index, detail };
+const detailByEmail = async(req,res) => {
+  
+  try {
+    const email = req.params.email;
+    const users = await UserModel.findOne({
+      where:{
+        email: email,
+      }
+    })
+    
+    if (users === null) {
+      return res.json({
+        status: "Fail",
+        msg: "Daftar User Tidak Ditemukan",
+      });
+    }
+    return res.json({
+      status: "Succses",
+      msg: "Daftar User Di Temukan ",
+      data: users,
+    });
+    res.send(email)    
+  } catch (err) {
+    return res.status(403).json({
+      status: "fail",
+      msg: "ada kesalahan",
+    });
+  }
+}
+const destroy= async (req,res) =>{
+  try {
+    const {id} = req.params;
+    const users = await UserModel.destroy({
+      where:{
+        id: id,
+      }
+    })
+    
+    if (users === 0) {
+      return res.json({
+        status: "Fail",
+        msg: "User Tidak Ditemukan",
+      });
+    }
+    return res.json({
+      status: "Succses",
+      msg: "User Di hapus ",
+      data: users,
+    });
+    res.send(email)    
+  } catch (err) {
+    return res.status(403).json({
+      status: "fail",
+      msg: "ada kesalahan",
+    });
+  }
+}
+
+const update = async (req,res) =>{
+  try {
+    const {id} = req.params;
+    const {name} = req.body;
+    const user = await UserModel.findByPk(id);
+    if (users === 0) {
+      return res.json({
+        status: "Fail",
+        msg: "User Tidak Ditemukan",
+      });
+    }
+    
+    await UserModel.update({
+      name:name,
+    },
+    {
+      where:{
+        id:id,
+      }
+    }
+    )
+  } catch (err) {
+    return res.status(403).json({
+      status: "fail",
+      msg: "ada kesalahan",
+    });
+  }
+
+  
+}
+
+module.exports = { index, detail ,detailByEmail ,destroy ,update };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { register } = require("../controller/AuthCotroller");
-const {index,detail}=require("../controller/UserControler")
+const { index, detail, detailByEmail, destroy, update } = require("../controller/UserControler");
 const UserModel = require("../models").user;
 const validationMiddleware = require("../middleware/validationMiddleware");
 const { validationRegister } = require("../validators/authValidator");
@@ -10,7 +10,10 @@ const { validationRegister } = require("../validators/authValidator");
 router.post("/register", validationRegister, validationMiddleware, register);
 
 //user
-router.get("/users",index)
-router.get("/users/:id",detail)
+router.get("/users", index);
+router.get("/users/:id", detail);
+router.get("/users/email/:email", detailByEmail);
+router.delete("/users/:id", destroy);
+router.put ("/users/update/:id",update)
 
 module.exports = router;
